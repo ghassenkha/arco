2901fd49c882d2963e909f3f040db05f609cd34e
ghassenkha <kha.ghassen@gmail.com>
2021-11-01 12:26:06 +0000

    commentaire

---
+++ migrations/Version20210802093141.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210802093141 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE uap ADD eff DOUBLE PRECISION NOT NULL, ADD ineff DOUBLE PRECISION NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE uap DROP eff, DROP ineff');
+    }
+}
